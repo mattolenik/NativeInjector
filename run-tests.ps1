d17980efbba44aa5d834d277c002021586ad885c
Matt Olenik <olenikm@gmail.com>
2016-01-27 23:22:20 -0800

    Fix test configuration

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,3 +1,3 @@
- .\packages\xunit.runner.console.*\tools\xunit.console.exe .\NativeInjector.test\bin\Debug\NativeInjector.Test.dll
+ .\packages\xunit.runner.console.*\tools\xunit.console.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll
  write-host
- .\packages\xunit.runner.console.*\tools\xunit.console.x86.exe .\NativeInjector.test\bin\Debug\NativeInjector.Test.dll
+ .\packages\xunit.runner.console.*\tools\xunit.console.x86.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll
