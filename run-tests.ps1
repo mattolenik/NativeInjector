6ac39a3790f7b109a3d6416297d6ecd7ffecd3c3
Matt Olenik <olenikm@gmail.com>
2016-01-27 23:37:40 -0800

    AppVeyor test support and delete runas64.exe

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,3 +1,4 @@
-.\packages\xunit.runner.console.*\tools\xunit.console.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll
+$reporter = @{$true="-appveyor";$false="-verbose"}[$CI -eq "True"]
+.\packages\xunit.runner.console.*\tools\xunit.console.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll $reporter
 write-host
-.\packages\xunit.runner.console.*\tools\xunit.console.x86.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll
+.\packages\xunit.runner.console.*\tools\xunit.console.x86.exe .\NativeInjector.test\bin\$env:CONFIGURATION\NativeInjector.Test.dll $reporter
